eab699c51aaccd9fbeefd2e84349a0be105d9f35
max <dikovmax04@gmail.com>
2024-11-06 16:28:05 +0100

    Added google analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,8 @@ import localFont from "next/font/local";
 import "./globals.css";
 import Header from "./header";
 import { Footer } from "./footer";
-
+import Script from "next/script";
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 export const metadata = {
   title: "TetroFlow - A blazing fast and open-source in-memory storage",
@@ -16,11 +17,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+      </head>
       <body className="antialiased flex flex-col min-h-screen">
         <Header />
         <div className="flex-grow bg-gray-50 mx-2 md:mx-0">{children}</div>
         <Footer />
       </body>
+      <GoogleAnalytics gaId="G-NF2BF9BGLT" />
     </html>
   );
 }
--- src/app/privacypolicy/page.js
+++ src/app/privacypolicy/page.js
@@ -16,40 +16,46 @@ export default function PrivacyPolicy() {
                         2. Data Collection and Use
                     </p>
                     Our website does not collect or store any cookies or track users for any form of profiling or marketing. The only information collected is the data automatically transmitted by your browser when accessing the website, such as browser headers, IP addresses, and request logs. This information is necessary for the basic functionality of the webserver and is only used to maintain and secure the website. We do not retain any sensitive or personal data.
-                    We strictly use this data for the purpose of administering the website, analyzing performance, and ensuring its security. Under no circumstances will we sell, rent, or distribute any information to third parties.
                 </div>
 
                 <div>
                     <p className="text-md font-semibold">
-                        3. Data Retention
+                        3. Use of Google Analytics
+                    </p>
+                    We use Google Analytics to help analyze website usage and performance. Google Analytics may collect various data, including IP addresses, browser information, and interaction details. Please note that Google Analytics tracking cannot be opted out of on this website, and we do not control or have access to the specific data Google collects or processes through this service. Therefore, we cannot take any responsibility for how Google handles, stores, or shares the information collected through Google Analytics. For more information on Google’s data collection practices, please review Google’s Privacy Policy directly.
+                </div>
+
+                <div>
+                    <p className="text-md font-semibold">
+                        4. Data Retention
                     </p>
                     The logs and basic server data mentioned above may be stored for a limited period for the purpose of analyzing server issues, ensuring security, and for routine maintenance. However, no personally identifiable information will be extracted from these logs.
                 </div>
 
                 <div>
                     <p className="text-md font-semibold">
-                        4. Images and Intellectual Property
+                        5. Images and Intellectual Property
                     </p>
                     All images displayed on our website were generated using artificial intelligence (AI). We do not claim any intellectual property rights or copyright over these images. You may use these images freely, but we do not provide any guarantees concerning their originality or exclusive use.
                 </div>
 
                 <div>
                     <p className="text-md font-semibold">
-                        5. Open Source and Third-Party Links
+                        6. Open Source and Third-Party Links
                     </p>
                     The website represents an open-source software project, and all data, including software, is made openly available to users. We do not guarantee the accuracy, functionality, or security of third-party websites linked from our platform. Accessing these third-party links is at the user&apos;s own risk, and we disclaim any liability for any harm or damages that may arise from their use.
                 </div>
 
                 <div>
                     <p className="text-md font-semibold">
-                        6. Liability
+                        7. Liability
                     </p>
                     We shall not be held responsible for any actions taken based on information, materials, or links found on the website. While we strive to provide accurate and up-to-date information, we do not warrant the completeness, reliability, or availability of the website or any of its content. Any reliance placed on such materials is strictly at the user&apos;s own risk.
                 </div>
 
                 <div>
                     <p className="text-md font-semibold">
-                        7. Contact Information
+                        8. Contact Information
                     </p>
                     For any inquiries regarding this Privacy Policy or the terms of use of our website, please contact us at:
                     <br />
